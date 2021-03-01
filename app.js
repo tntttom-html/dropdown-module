765df6020c70fdac399a0c14344b6996ff73f514
tntttom <tntt_tom@me.com>
2021-02-28 22:37:17 -0600

    updated

--- app.js
+++ app.js
@@ -7,21 +7,13 @@ $(document).ready(function () {
 
   $("#purpose").on("change", function () {
     console.log($("#recruiting"));
-    if (this.value == "recruiting") {
-      contentList.map((content) => {
-        content.classList.remove("content-active");
-      });
-      $("#recruiting")[0].classList.add("content-active");
-    } else if (this.value == "onboarding") {
-      contentList.map((content) => {
-        content.classList.remove("content-active");
-      });
-      $("#onboarding")[0].classList.add("content-active");
-    } else if (this.value == "permits") {
-      contentList.map((content) => {
-        content.classList.remove("content-active");
-      });
-      $("#permits")[0].classList.add("content-active");
+    for (let i = 0; i < contentList.length; i++) {
+      if (this.value == i + 1) {
+        contentList.map((content) => {
+          content.classList.remove("content-active");
+        });
+        contentList[i].classList.add("content-active");
+      }
     }
   });
 });
